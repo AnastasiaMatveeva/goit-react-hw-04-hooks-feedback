f979b9f258a9a3dc80f0a784e19d0656b1dba1cc
unknown <anastasiagieieva@gmail.com>
2021-08-17 11:35:11 +0300

    Updates

---
+++ precache-manifest.83e00d0f7a4b0a856928bd5cd074b724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521fc788413abaa96385facc7104ee2e",
+    "url": "/goit-react-hw-04-hooks-feedback/index.html"
+  },
+  {
+    "revision": "81f3f32fa759fd9bb710",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "7ec4a03d9e2301a3f44a",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/main.e9ef095a.chunk.css"
+  },
+  {
+    "revision": "81f3f32fa759fd9bb710",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.54f98ffe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.54f98ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec4a03d9e2301a3f44a",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/main.2aa88735.chunk.js"
+  },
+  {
+    "revision": "11f0970e892669ab6384",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/runtime-main.92b7376c.js"
+  }
+]);
